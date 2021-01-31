2acca44abbfba2816e3c755d6b8040b94767a03e
Vladimir Shubarin <vhundef@gmail.com>
2021-01-31 15:32:15 +0300

    Fixed problems with textures
    Took 48 minutes

--- texture.hpp
+++ texture.hpp
@@ -26,14 +26,16 @@ public:
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 // load and generate the texture
+        stbi_set_flip_vertically_on_load(1);
         unsigned char *data = stbi_load(_filepath.c_str(), &width, &height, &nrChannels, 0);
-        if (data)
-        {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        if (data) {
+            if (nrChannels == 3) {
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            } else {
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+            }
             glGenerateMipmap(GL_TEXTURE_2D);
-        }
-        else
-        {
+        } else {
             LOG_S(WARNING) << "Failed to load texture";
         }
         stbi_image_free(data);
